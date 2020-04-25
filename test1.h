bae1ce11cc7ca20a141d31d88a47249eb3fc6c9e
Egor <EGOR@MAIL.RU>
2020-04-25 12:19:11 +0300

    code:test1.h

---
+++ test1.h
@@ -0,0 +1,14 @@
+
+#ifndef TEST1_H_INCLUDED
+#define TEST1_H_INCLUDED
+#include <iostream>
+#include <vector>
+
+void test_positive();
+void test_similar ();
+void test_negative();
+void test_one_number();
+void test_empty();
+void test_lishnya_liniya();
+void test_bins0();
+#endif // TEST1_H_INCLUDED
