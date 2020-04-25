b605e477acf3ac1b7bc52ab483ad48d1945ef5ed
Egor <EGOR@MAIL.RU>
2020-04-25 12:18:55 +0300

    code:test.cpp

--- test.cpp
+++ test.cpp
@@ -1,51 +1,6 @@
 #include "histogram.h"
-
+#include "test1.h"
 #include <cassert>
-
-void
-test_positive() {
-    double min = 0;
-    double max = 0;
-    find_minmax({1, 2, 3}, min, max);
-    assert(min == 1);
-    assert(max == 3);
-}
-
-void test_similar ()
-{
-    double min = 0;
-    double max = 0;
-    find_minmax({2, 2, 2}, min, max);
-    assert(min == 2);
-    assert(max == 2);
-}
-
-void test_negative()
-{
-    double min = 0;
-    double max = 0;
-    find_minmax({-1, -2, -3}, min, max);
-    assert(min == -3);
-    assert(max == -1);
-}
-
-void test_one_number()
-{
-    double min = 0;
-    double max = 0;
-    find_minmax({1}, min, max);
-    assert(min == 1);
-    assert(max == 1);
-}
-
-void test_empty()
-{
-    double min = 0;
-    double max = 0;
-    find_minmax({}, min, max);
-    assert(min ==0);
-    assert(max == 0);
-}
 
 int
 main()
@@ -54,4 +9,6 @@ main()
     test_negative ();
     test_similar ();
     test_one_number();
+    test_lishnya_liniya();
+    test_bins0();
 }
