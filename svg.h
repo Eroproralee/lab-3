c7988034b9fd2c7517f030886975b42184055333
alice <alice@example.com>
2020-05-08 12:13:22 +0300

    code: окончание пункта 3.1.2 h

--- svg.h
+++ svg.h
@@ -8,7 +8,7 @@ void svg_end();
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black");
 void svg_line(double x1,double y1,double x2,double y2,double length_ch,double length_pr,string stroke_1 = "red");
-void show_histogram_svg(const vector<size_t>& bins,size_t bin_count,double length_ch,double length_pr);
+void show_histogram_svg(const vector<size_t>& bins );
 
 
 #endif // SVG_H_INCLUDED
