826dbd131bc7ada290ec1f693a2e2bdae39c8a1e
alice <alice@example.com>
2020-05-08 15:28:22 +0300

    code:svg.h 3.2.9

--- svg.h
+++ svg.h
@@ -1,14 +1,14 @@
 #ifndef SVG_H_INCLUDED
 #define SVG_H_INCLUDED
-#include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
+void svg_rect(const auto x, double y,const auto width, double height);
 void svg_begin(double width, double height);
 void svg_end();
-void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black");
-void svg_line(double x1,double y1,double x2,double y2,double length_ch,double length_pr,string stroke_1 = "red");
-void show_histogram_svg(const vector<size_t>& bins );
+void svg_text(double left,double baseline, string text);
 
+void show_histogram_svg(const vector<size_t>& bins);
+void show_histogram_text(const vector<size_t>& bins);
 
 #endif // SVG_H_INCLUDED
