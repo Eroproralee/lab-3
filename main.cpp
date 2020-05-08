34862c992be7e6e426d2333dbfdbcd83886deabe
alice <alice@example.com>
2020-05-08 09:27:53 +0300

    code:istream

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "svg.h"
 using namespace std;
 
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers( istream& in ,  size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -41,7 +41,7 @@ vector <size_t>  make_histogram(const vector<double> &numbers,size_t bin_count)
         size_t number_count;
         cerr << "Enter number count: ";
         cin >> number_count;
-        const auto numbers = input_numbers(number_count);
+        const auto numbers = input_numbers(cin ,number_count);
         size_t bin_count;
         cerr << "Enter column count: ";
         cin >> bin_count;
