58d82e4c2da95c5dc4c0b5777ed0f4126c4b09c8
alice <alice@example.com>
2020-05-08 11:02:08 +0300

    code:заменяем ввод данных в мэйин и передаём в make hist...

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
-using namespace std;
 
-vector<double> input_numbers( istream& in ,  size_t count)
+using namespace std;
+vector<double> input_numbers(istream&in,size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
@@ -14,40 +14,44 @@ vector<double> input_numbers( istream& in ,  size_t count)
     return result;
 }
 
-vector <size_t>  make_histogram(const vector<double> &numbers,size_t bin_count)
-{
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-    double min;
-    double max;
+vector<size_t> make_histogram(size_t count,const vector<double>& numbers)
+{   double min=0;
+    double max=0;
     find_minmax(numbers,min,max);
-    vector<size_t> bins(bin_count,0);
-    for (double number : numbers)
-    {
-        size_t bin;
-        bin = (number - min) / (max - min) * bin_count;
-        if (bin == bin_count)
+    vector<size_t> bins(count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * count);
+        if (bin == count)
         {
             bin--;
         }
         bins[bin]++;
     }
-
     return bins;
 }
-    int main()
-    {
-        double length_ch,length_pr;
-        size_t number_count;
-        cerr << "Enter number count: ";
-        cin >> number_count;
-        const auto numbers = input_numbers(cin ,number_count);
-        size_t bin_count;
-        cerr << "Enter column count: ";
-        cin >> bin_count;
-        double min, max;
-        find_minmax(numbers,min,max);
-        const auto bins = make_histogram(numbers, bin_count);
-        show_histogram_svg(bins,bin_count,length_ch,length_pr);
-        return 0;
-    }
+struct Input
+{vector<double>numbers;
+size_t bin_count;
+};
+Input read_input(istream& in)
+{
+ Input data;
+ cerr << "Enter number count: ";
+ size_t number_count;
+ cin >> number_count;
+ cerr <<"Enter numbers:";
+ data.numbers = input_numbers(in,number_count);
+ size_t bin_count;
+ cin >>data.bin_count;
+ return data;
+ }
+int main()
+{
+    double length_ch,length_pr;
+    Input data;
+    data=read_input(cin);
+    const auto bins =make_histogram(data.bin_count,data.numbers);
+
+    show_histogram_svg(bins,data.bin_count,length_ch,length_pr);
+    return 0;
+}
