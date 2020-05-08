56f15d9f5994dd37c9fc841c3ea9f19fbf3ef788
alice <alice@example.com>
2020-05-08 11:58:41 +0300

    code : изменение гистограмы.cpp

--- histogram.cpp
+++ histogram.cpp
@@ -1,25 +1,29 @@
 #include "histogram.h"
-void find_minmax(const vector<double> &numbers, double& min, double& max)
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+struct Input
+{vector<double>numbers;
+size_t bin_count;
+};
+void find_minmax(struct Input Input,double& min,double& max)
+{
+    int q=Input.numbers.size();
+    if(q!=0)
     {
-    if (numbers.size()==0)
-    {
-        return;
-    }
-    else
-    {
-
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
+        min = Input.numbers[0];
+        max = Input.numbers[0];
+        for (double number : Input.numbers)
         {
-        if (number < min)
+            if (number < min)
             {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
         }
     }
-    }
+}
