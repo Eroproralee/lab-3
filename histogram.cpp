98f15a8a42e75c80aa5f54de827cb53511de5f6f
Egor <EGOR@MAIL.RU>
2020-05-20 15:40:12 +0300

    code:histogram.cpp

--- histogram.cpp
+++ histogram.cpp
@@ -1,16 +1,23 @@
-#include "histogram.h"
-void find_minmax(const vector<double> &numbers, double& min, double& max)
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+struct Input
+{vector<double>numbers;
+size_t bin_count;
+};
+void find_minmax(struct Input data , double& min, double& max)
     {
-    if (numbers.size()==0)
+    if (data.numbers.size()==0)
     {
         return;
     }
     else
     {
 
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
+    min = data.numbers[0];
+    max = data.numbers[0];
+    for (double number : data.numbers)
         {
         if (number < min)
             {
