b5d3fd5c1a9bd3d1f587f7a13e571162a3c90c89
Egor <EGOR@MAIL.RU>
2020-05-22 19:10:12 +0300

    code:main.cpp

--- main.cpp
+++ main.cpp
@@ -1,118 +1,123 @@
 #include <iostream>
 #include <vector>
-#include "histogram.h"
-#include "svg.h"
+#include <string>
 #include <curl/curl.h>
 #include <sstream>
-#include <string>
-#include <sstream>
-#include <string>
+#include "histogram.h"
+#include "svg.h"
+#include <windows.h>
+#include <stdio.h>
+
 using namespace std;
-string
-make_info_text() {
-    stringstream buffer;
-    string Windows = "Windows v6.3" ;
-    string name = "Egor" ;
-    buffer << "versin " << Windows ;
-    buffer << "name " << name ;
-    // TODO: получить имя компьютера, записать в буфер.
-    return buffer.str();
-}
 struct Input {
     vector<double> numbers;
     size_t bin_count;
 };
-vector<double> input_numbers(istream&in,size_t count)
-{
-vector<double> result(count);
-for (size_t i = 0; i < count; i++)
-{
-in >> result[i];
-}
-return result;
+vector<double>
+input_numbers(istream& in, size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        in >> result[i];
+    }
+    return result;
 }
 
-vector<size_t> make_histogram(struct Input data)
-{   double min=0;
-    double max=0;
-    find_minmax(data,min,max);
-    vector<size_t> bins(data.bin_count);
-    for (double number : data.numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * data.bin_count);
-        if (bin == data.bin_count)
+Input
+read_input(istream& in , bool prompt ) {
+    Input data;
+if ( prompt == true ){
+    cerr << "Enter number count: ";
+}
+    size_t number_count;
+    cin >> number_count;
+
+   if ( prompt == true ){ cerr << "Enter numbers: ";
+   }
+    data.numbers = input_numbers(in, number_count);
+ cerr << "Enter bin ncount: ";
+    size_t bin_count;
+    cin >> bin_count;
+    data.bin_count = bin_count  ;
+
+    return data;
+}
+
+
+vector <size_t>  make_histogram(struct Input input)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+    double min;
+    double max;
+    find_minmax(input,min,max);
+    vector<size_t> bins(input.bin_count,0);
+    for (double number : input.numbers)
+    {
+        size_t bin;
+        bin = (number - min) / (max - min) * input.bin_count;
+        if (bin == input.bin_count)
         {
             bin--;
         }
         bins[bin]++;
     }
+
     return bins;
 }
-Input read_input(istream& in, bool prompt)
+write_data(void* items, size_t item_size, size_t item_count, void* ctx)
 {
-Input data;
-if(prompt == true )
-{cerr << "Enter number count: ";
-cerr << "Enter bin_count : " ;
-}
-size_t number_count;
-cin >> number_count;
-data.numbers = input_numbers(cin,number_count);
-size_t bin_count;
-cin >>data.bin_count;
-return data;
-}
-size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
- {
-
-     stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
-
-    const char* item = reinterpret_cast<const char*>(items);
-    size_t data_size;
-
-    data_size=item_size* item_count;
-
-
-    buffer->write(item, data_size);
+    const size_t data_size = item_size * item_count;
+    const char* new_items = reinterpret_cast<const char*>(items);
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(new_items, data_size);
     return data_size;
-
 }
-Input download(const string& address)
- {
-     stringstream buffer;
- CURL *curl = curl_easy_init();
+Input
+download(const string& address)
+{
+    stringstream buffer;
 
-    CURLcode res;
-  curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-  res = curl_easy_perform(curl) ;
-  if(res!=0)
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL *curl = curl_easy_init();
+    if(curl)
     {
-    cout<<curl_easy_strerror(curl_easy_perform(curl));
-    exit(1);
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+
+        /* Perform the request */
+        res = curl_easy_perform(curl);
+
+        if(!res)
+        {
+            /* check the size */
+            double dl;
+            res = curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD, &dl);
+            if(!res)
+            {
+                cerr << "Downloaded:"<<dl<<"bytes\n";
+            }
+        }
     }
-    curl_easy_cleanup(curl);
-
-    return read_input(buffer, false) ;
 }
-
-
-
-int
-main(int argc, char* argv[])
-{   Input input;
-    if(argc>1)
+    int main(int argc, char* argv[])
     {
-     input = download(argv[1]);
+        Input input ;
+         if(argc>1)
+    {
+        input = download(argv[1]);
     }
     else
     {
-     input=read_input(cin,true);
+        input=read_input(cin,true);
     }
-    curl_global_init(CURL_GLOBAL_ALL);
-    double length_pr , length_ch ;
-const auto data=read_input(cin, true);
-const auto bins =make_histogram(data);
-show_histogram_svg(bins , length_pr , length_ch );
-return 0;
-}
+
+        double length_ch,length_pr;
+    const auto bins = make_histogram(input);
+
+        show_histogram_svg(bins,length_ch,length_pr);
+        return 0;
+    }
