60e6610eb9f92f959cd1e4fc6609f9f492fa71b9
Egor <EGOR@MAIL.RU>
2020-05-23 05:41:56 +0300

    code:svg.h

--- svg.h
+++ svg.h
@@ -1,16 +1,26 @@
 #ifndef SVG_H_INCLUDED
 #define SVG_H_INCLUDED
-#include <iostream>
 #include <vector>
+#include <string>
+#include <windows.h>
+#include <stdio.h>
+
 using namespace std;
+
+DWORD WINAPI GetVersion(void);
+
+string make_info_text( double top  );
+string make_info_text1();
+string make_info_text2();
+void svg_text1(double left, double baseline, string text);
 void svg_begin(double width, double height);
 void svg_end();
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black");
 void svg_line(double x1,double y1,double x2,double y2,double length_ch,double length_pr,string stroke_1 = "red");
-void show_histogram_svg(const vector<size_t>& bins);
+void show_histogram_svg(const vector<size_t>& bins  ,   double length_pr , double  length_ch) ;
 
-double height_counter (size_t max_asteriks ,  size_t max_count , size_t bin  );
+
 
 
 #endif // SVG_H_INCLUDED
